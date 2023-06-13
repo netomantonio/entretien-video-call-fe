7ca8e4f0dd08f79f07308047f54dbe0f6d763c17
[NETO] Antonio Miranda <netomantonio@gmail.com>
2023-06-12 20:28:03 -0400

    first commit application base in openvidu-webcomponent

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# entretien-video-call-fe
+<h1 align="center">Welcome to entretien-video-call-fe 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://docs.entretienrh.com.br/video-call" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">
+    <img alt="License: GNU General Public License v3.0" src="https://img.shields.io/badge/License-GNU General Public License v3.0-yellow.svg" />
+  </a>
+</p>
+
+> Esta aplicação é parte da plataforma de gerenciamento de entrevistas por competência entretien-rh e sua responsabilidade é executar a video chamada para as entrevistas agendadas.
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+use o comando http-server para iniciar a aplicação
+```
+
+## Author
+
+👤 **ENTRETIEN-RH**
+
+* Website: https://entretienrh.com.br
+* Github: [@netomantonio](https://github.com/netomantonio)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2023 [ENTRETIEN-RH](https://github.com/netomantonio).<br />
+This project is [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.html) licensed.
---
+++ app.js
@@ -0,0 +1,139 @@
+$(document).ready(async () => {
+    const webComponent = document.querySelector('openvidu-webcomponent');
+
+    webComponent.addEventListener('onSessionCreated', (event) => {
+        const session = event.detail;
+
+        // You can see the session documentation here
+        // https://docs.openvidu.io/en/stable/api/openvidu-browser/classes/session.html
+
+        session.on('connectionCreated', (e) => {
+            console.log("connectionCreated", e);
+        });
+
+        session.on('streamDestroyed', (e) => {
+            console.log("streamDestroyed", e);
+        });
+
+        session.on('streamCreated', (e) => {
+            console.log("streamCreated", e);
+        });
+
+        session.on('sessionDisconnected', (event) => {
+            console.warn("sessionDisconnected", event);
+            window.location.replace("http://localhost:3000/interviews")
+            window.close()
+        });
+
+        session.on('exception', (exception) => {
+            console.error(exception);
+        });
+    });
+
+    webComponent.addEventListener('onJoinButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarLeaveButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarCameraButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarMicrophoneButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarScreenshareButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarParticipantsPanelButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarChatPanelButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarFullscreenButtonClicked', (event) => { });
+    webComponent.addEventListener('onParticipantCreated', (event) => { });
+
+});
+
+async function joinSession(sessionId, tokenServerApp, interviewId) {
+
+    // Requesting tokens
+    const promiseResults = await Promise.all([getToken(sessionId, tokenServerApp, interviewId), getToken(sessionId, tokenServerApp, interviewId)]);
+    const tokens = {webcam: promiseResults[0], screen: promiseResults[1]};
+
+    //Getting the webcomponent element
+    const webComponent = document.querySelector('openvidu-webcomponent');
+
+
+    // Displaying webcomponent
+    webComponent.style.display = 'block';
+
+    // webComponent.participantName = participantName;
+
+    // You can see the UI parameters documentation here
+    // https://docs.openvidu.io/en/stable/api/openvidu-angular/components/OpenviduWebComponentComponent.html#inputs
+
+    // webComponent.toolbarScreenshareButton = false;
+    // webComponent.minimal = true;
+    // webComponent.prejoin = true;
+
+    // webComponent.videoMuted = false;
+    // webComponent.audioMuted = false;
+
+    // webComponent.toolbarScreenshareButton = true;
+    // webComponent.toolbarFullscreenButton = true;
+    // webComponent.toolbarLeaveButton = true;
+    // webComponent.toolbarChatPanelButton = true;
+    // webComponent.toolbarParticipantsPanelButton = true;
+    webComponent.toolbarDisplayLogo = false;
+    webComponent.toolbarCaptionsButton = false;
+    webComponent.toolbarBackgroundEffectsButton = false;
+    // webComponent.toolbarDisplaySessionName = true;
+    // webComponent.streamDisplayParticipantName = true;
+    // webComponent.streamDisplayAudioDetection = true;
+    // webComponent.streamSettingsButton = true;
+    webComponent.participantPanelItemMuteButton = true;
+
+    webComponent.tokens = tokens;
+}
+
+
+/**
+ * --------------------------------------------
+ * GETTING A TOKEN FROM YOUR APPLICATION SERVER
+ * --------------------------------------------
+ * The methods below request the creation of a Session and a Token to
+ * your application server. This keeps your OpenVidu deployment secure.
+ *
+ * In this sample code, there is no user control at all. Anybody could
+ * access your application server endpoints! In a real production
+ * environment, your application server must identify the user to allow
+ * access to the endpoints.
+ *
+ * Visit https://docs.openvidu.io/en/stable/application-server to learn
+ * more about the integration of OpenVidu in your application server.
+ */
+const APPLICATION_SERVER_URL = "http://localhost:5000/openvidu/";
+
+function getToken(customSessionId, tokenServerApp, interviewId) {
+    return createSession(customSessionId, tokenServerApp, interviewId).then(sessionId => createToken(sessionId, tokenServerApp));
+}
+
+function createSession(sessionId, tokenServerApp, interviewId) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "POST",
+            url: APPLICATION_SERVER_URL + 'api/sessions/'  + interviewId ,
+            data: JSON.stringify({ customSessionId: sessionId }),
+            headers: {
+                "Content-Type":"application/json",
+                "Authorization": "Bearer " + tokenServerApp
+            },
+            success: response => resolve(response), // The sessionId
+            error: (error) => reject(error)
+        });
+    });
+}
+
+function createToken(sessionId, tokenServerApp) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: 'POST',
+            url: APPLICATION_SERVER_URL + 'api/sessions/' + sessionId + '/connections',
+            data: JSON.stringify({}),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ` + tokenServerApp
+            } ,
+            success: (response) => resolve(response), // The token
+            error: (error) => reject(error)
+        });
+    });
+}
